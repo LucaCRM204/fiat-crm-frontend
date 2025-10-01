a1522e14e10a1a55cecad78656fec2103d03d419
Luca Caorsi <administracion@allumapublicidad.com.ar>
2025-09-30 21:41:22 -0300

    Fix: Add explicit .js extension to App import

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './App.js'  // ← Agregar .js explícitamente
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
